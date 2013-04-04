7ac4f4740a5ac052e9f93516f0dee8360aa42e9b
Tilmann Bruckhaus <tilmann@outright.com>
2013-04-04 11:59:21 -0700

    rank suggestions by length

--- bin/turfie.rb
+++ bin/turfie.rb
@@ -8,12 +8,14 @@ class Turfie
   require_relative 'thesaurus'
 
   def initialize
-    @parser = Parser.new
+    @parser      = Parser.new
+    @suggestions = []
   end
 
   def run
     Cli.print_welcome
     loop do
+      @suggestions = []
       user_input = Cli.get_input
       break if user_input.empty?
       suggest(user_input)
@@ -22,25 +24,37 @@ class Turfie
 
   def suggest(user_input)
     word_arrays = @parser.get_word_splits(user_input)
+    suggest_for_word_arrays(word_arrays)
+    rank_suggestions
+    show_suggestions
+  end
+
+  def show_suggestions
+    puts 'You may like these suggestions:'
+    @suggestions.each_with_index { |suggestion, rank| puts "  #{rank}. #{suggestion}"}
+  end
+
+  def suggest_for_word_arrays(word_arrays)
     word_arrays.each do |word_array|
       synonym_array = Thesaurus.get_synonyms_array(word_array)
-      show_suggestions(synonym_array)
+      suggest_for_synonym_array(synonym_array)
     end
   end
 
-  def show_suggestions(synonym_array, prefix = '')
-    puts 'You may like these suggestions:' if prefix == ''
+  def suggest_for_synonym_array(synonym_array, prefix = '')
     if synonym_array.length == 1
-      puts '... or you may like these suggestions:' if prefix != ''
       synonym_array[0].each do |synonym|
-        puts "#{prefix}#{Parser.sanitize(synonym)}"
+        @suggestions << "#{prefix}#{Parser.sanitize(synonym)}"
       end
     else
       synonym_array[0].each do |synonym|
-        show_suggestions(synonym_array[1..-1], prefix + Parser.sanitize(synonym))
-        puts
+        suggest_for_synonym_array(synonym_array[1..-1], prefix + Parser.sanitize(synonym))
       end
     end
+  end
+
+  def rank_suggestions
+    @suggestions.sort_by! { |x| x.length }
   end
 
 end
