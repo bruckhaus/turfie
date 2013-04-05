73ab221aed641add011e8b1031e144528190baef
Tilmann Bruckhaus <tilmann@outright.com>
2013-04-04 21:07:25 -0700

    load and sanitize TLDs

--- bin/tld.rb
+++ bin/tld.rb
@@ -6,12 +6,28 @@ class Tld
     @tlds = load
   end
 
+  def show
+    puts "TLDs: #{@tlds}"
+  end
+
   def load
-    tld_file = File.dirname(__FILE__) + '/../data/tld/strings-1200utc-13jun12-en.csv'
-    puts "\n"
-    CSV.foreach(tld_file, :quote_char => '"', :col_sep =>',', :row_sep =>:auto) do |row|
-      print "#{row[0]}, "
+    tlds = []
+    CSV.foreach(tld_file, :quote_char => '"', :col_sep => ',', :row_sep => :auto) do |row|
+      tld = row[0]
+      tlds << tld
     end
+    sanitize(tlds)
+  end
+
+  # remove field from header row, make unique, sort, and downcase
+  def sanitize(tlds)
+    tlds = tlds[1..-1]
+    tlds.uniq!.sort!
+    tlds.map { |tld| tld.downcase }
+  end
+
+  def tld_file
+    File.dirname(__FILE__) + '/../data/tld/strings-1200utc-13jun12-en.csv'
   end
 
 end
